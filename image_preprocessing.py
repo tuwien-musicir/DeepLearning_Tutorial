2ea529d48efe4bb89cb86e96041f654448a30572
Thomas Lidy <lidy@ifs.tuwien.ac.at>
2017-01-10 12:15:20 +0100

    corrected a bug in resize and crop where additional borders were added to the sides when aspect ratio is different

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -17,31 +17,31 @@ def reorder_RGB(data, dim_ordering='default'):
     return data
 
 
-
 def resize_and_crop(img,target_width=224,target_height=224):
     width, height = img.size
 
-    # 1) resize the larger side proportional to the target of the smaller side (will be taller if not same aspect)
+    img_ratio = width / float(height)
+    target_ratio = target_width / float(target_height)
+
+    # 1) compare ratios and resize the larger side proportional to the target of the smaller side
     new_width, new_height = (target_width, target_height)
 
-    if height > width:
-        new_height = int(height * (target_width / float(width)))
+    if target_ratio > img_ratio:
+        new_height = int(round(height * (target_width / float(width))))
     else:
-        new_width = int(width * (target_height / float(height)))
+        new_width = int(round(width * (target_height / float(height))))
 
     img_new = img.resize((new_width, new_height), Image.ANTIALIAS)
     # The filter argument can be one of NEAREST (use nearest neighbour), BILINEAR (linear interpolation in a
     # 2x2 environment), BICUBIC (cubic spline interpolation in a 4x4 environment), or ANTIALIAS (a high-quality downsampling filter).
-    # If omitted, or if the image has mode "1" or "P", it is set to NEAREST.
+    # If omitted, or if the image has mode "1" or “P”, it is set to NEAREST.
     # Note that the bilinear and bicubic filters in the current version of PIL are not well-suited for large downsampling
     # ratios (e.g. when creating thumbnails). You should use ANTIALIAS unless speed is much more important than quality.
 
     # 2) crop to target size
-
     # offset to half of the remaining padding (one of them is 0)
     width_offset  = (new_width - target_width) / 2
     height_offset = (new_height - target_height) / 2
-    width_offset, height_offset
 
     # crop with offsets
     img_new = img_new.crop((width_offset, height_offset, width_offset+target_width, height_offset+target_height))
